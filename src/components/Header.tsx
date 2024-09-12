6d3784d3be58e7ad17b5724db7279d72e7dc18db
reito <zhenlinshannei@gmail.com>
2024-09-13 02:44:39 +0900

    add title

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,10 @@ import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
-    <header className="bg-blue-950 text-white p-4">
+    <header className="bg-blue-950 text-white p-4 flex justify-between items-center">
+      <h1 className="text-lg font-bold">
+        <Link to="/">求人検索アプリ</Link>
+      </h1>
       <nav>
         <ul className="flex justify-end space-x-4">
           <li><Link to="/">求人検索</Link></li>
